f33955aee406a6c41a03f70123ece07251b7cb78
jiahangLee <lijiahang997>
2019-03-08 21:37:12 +0800

    【数据结构】-动态数组

---
+++ src/main/java/data/Array.java
@@ -0,0 +1,115 @@
+package data;
+
+import java.util.Arrays;
+
+/**
+ * @Auther: jiahangLee
+ * @Date: 2019/3/8 15:45
+ * @Description: //TODO
+ * @version: V1.0
+ */
+public class Array<E> {
+    private E[] data;
+    private int size;
+
+    public Array(int capacity) {
+        data = (E[]) new Object[capacity];
+        size = 0;
+    }
+    public Array() {
+        this(10);
+    }
+    public int getSize() {
+        return size;
+    }
+    public int getCapacity() {
+        return data.length;
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    public void addLast(E e) {
+//        if(size == data.length)
+//            throw new IllegalArgumentException("数组满了");
+//        data[size] = e;
+//        size++;
+        insert(size,e);
+    }
+    public void addFirst(E e) {
+        insert(0,e);
+    }
+    public void insert(int index,E e) {
+        if(size == data.length)
+            resize(2*size);
+        if(index < 0 || index >size)
+            throw new IllegalArgumentException("插入位置不正确");
+        for(int i = size-1;i >= index;i--) {
+            data[i+1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
+    public E get(int index) {
+        if(index<0 || index>=size)
+            throw new IllegalArgumentException("查询位置不正确");
+        return data[index];
+    }
+    public void set(int index,E e) {
+        if(index<0 || index>size)
+            throw new IllegalArgumentException("修改位置不正确");
+        data[index] = e;
+    }
+    public boolean contains(E e) {
+        for(E x:data){
+            if(x.equals(e))
+                return true;
+        }
+        return false;
+    }
+    public int find(E e) {
+        for(int i=0;i<size;i++) {
+            if(data[i].equals(e))
+                return i;
+        }
+        return -1;
+    }
+    public E remove(int index) {
+        if(index<0 || index >= size) {
+            throw new IllegalArgumentException("删除位置不正确");
+        }
+        E ret = data[index];
+        for(int i = index;i < size-1;i++) {
+            data[i] = data[i+1];
+        }
+        size--;
+        data[size] = null;
+        if(size == data.length/2)
+            resize(data.length/2);
+        return ret;
+    }
+    public E removeFirst() {
+        return remove(0);
+    }
+    public E removeLast() {
+        return remove(size-1);
+    }
+    public void removeElement(E e) {
+        int index = find(e);
+        if(index != -1)
+            remove(index);
+    }
+    private void resize(int newCapacity) {
+        E[] newData = (E[])new Object[newCapacity];
+        for(int i=0;i<size;i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+    @Override
+    public String toString() {
+        return "Array{" +
+                "data=" + Arrays.toString(data) +
+                ", size=" + size +
+                '}';
+    }
+}
---
+++ src/main/java/data/MyMain.java
@@ -0,0 +1,42 @@
+package data;
+
+/**
+ * @Auther: jiahangLee
+ * @Date: 2019/3/8 16:26
+ * @Description: //TODO
+ * @version: V1.0
+ */
+public class MyMain {
+
+    public static void main(String[] args) {
+//        Array<Integer> arr = new Array<Integer>(20);
+//        for(int i=0;i < 10;i++) {
+//            arr.addLast(i);
+//        }
+//        System.out.println(arr);
+//        arr.insert(1,100);
+//        System.out.println(arr);
+//        arr.addFirst(99);
+//        System.out.println(arr);
+//        System.out.println(arr.get(3));
+//        arr.set(3,19);
+//        System.out.println(arr.get(3));
+
+//        arr.remove(1);
+//        System.out.println(arr);
+//        arr.removeElement(4);
+//        System.out.println(arr);
+//        arr.removeFirst();
+//        System.out.println(arr);
+
+        Array<Integer> arr = new Array<Integer>(10);
+        for(int i=0;i < 10;i++) {
+            arr.addLast(i);
+        }
+//        arr.addLast(new Student("bob",100));
+//        arr.addLast(new Student("alice",99));
+        System.out.println(arr);
+        arr.addLast(99);
+        System.out.println(arr);
+    }
+}
